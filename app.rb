b757ed9ecc78181246b6b99dc415fd8a07011df1
itggot-emil-lindblad <emil.lindblad@elev.ga.ntig.se>
2019-03-14 10:09:30 +0100

    Changes column names in database

--- app.rb
+++ app.rb
@@ -8,11 +8,12 @@ enable :sessions
 #arr.any
 #request
 configure do
-    set :securedpaths, ["/profile/*","/newpost","/edit/*"] #TODO fix "sub routes"
+    set :securedpaths, ["/profile/*","/newpost","/edit"]
     set :allowedfiles, [".jpg",".jpeg",".png"]
 end
 
 before do
+    p request.path_info
     if settings.securedpaths.include?(request.path_info)
         if session[:username] != nil
             break
@@ -25,7 +26,7 @@ end
 get('/') do 
     db = SQLite3::Database.new("db/blog.db")
     db.results_as_hash = true
-    blogposts = db.execute("SELECT blogposts.id, blog_title, blog_text, img_path, author_id, Username FROM blogposts INNER JOIN users on users.id = blogposts.author_id ORDER BY blogposts.id DESC")
+    blogposts = db.execute("SELECT blogposts.Id, BlogTitle, BlogText, ImgPath, AuthorId, Username FROM blogposts INNER JOIN users on users.Id = blogposts.AuthorId ORDER BY blogposts.Id DESC")
     slim(:index, locals:{blogposts: blogposts})
 end
 
@@ -74,7 +75,7 @@ end
 post('/register') do
     db = SQLite3::Database.new("db/blog.db")
     hashedpassword = BCrypt::Password.create(params["password"])
-    if db.execute("Select username FROM users WHERE username =?",params["username"]) != []
+    if db.execute("Select Username FROM users WHERE Username =?",params["username"]) != []
 		params[:usernameerror] = true
         redirect('/newuser')
 	else
@@ -88,7 +89,7 @@ end
 get('/profile/:id') do
     db = SQLite3::Database.new('db/blog.db')
     db.results_as_hash = true
-    blogposts = db.execute("SELECT blogposts.id, blog_title, blog_text, img_path, author_id, Username FROM blogposts INNER JOIN users on users.id = blogposts.author_id WHERE author_id = ? ORDER BY blogposts.id DESC",params["id"])
+    blogposts = db.execute("SELECT blogposts.Id, BlogTitle, BlogText, ImgPath, AuthorId, Username FROM blogposts INNER JOIN users on users.Id = blogposts.AuthorId WHERE AuthorId = ? ORDER BY blogposts.Id DESC",params["id"])
     slim(:myprofile, locals:{blogposts: blogposts})
 end
 
@@ -105,7 +106,7 @@ post('/newpost') do
         File.open("public/img/#{newname}", 'wb') do |f|
             f.write(img.read)
         end
-        db.execute("INSERT INTO blogposts(blog_title, blog_text, author_id, img_path) VALUES (?,?,?,?)",params["blog_title"],params["blog_text"],session[:userid],newname)
+        db.execute("INSERT INTO blogposts(BlogTitle, BlogText, AuthorId, ImgPath) VALUES (?,?,?,?)",params["blog_title"],params["blog_text"],session[:userid],newname)
         redirect("/profile/#{session[:userid]}")
 else
         "Please submit a picture"
@@ -115,34 +116,33 @@ end
 get('/edit/:id') do 
     db = SQLite3::Database.new('db/blog.db')
     db.results_as_hash = true
-    result = db.execute("SELECT id, blog_title, blog_text FROM blogposts WHERE id = ?",params["id"])
+    result = db.execute("SELECT Id, BlogTitle, BlogText FROM blogposts WHERE Id = ?",params["id"])
     slim(:editpost, locals:{result: result})
 end
 
 post('/edit/:id/update') do 
     db = SQLite3::Database.new('db/blog.db')
-    db.execute("UPDATE blogposts SET blog_title = ?,blog_text = ? WHERE id = ?",params["blog_title"],params["blog_text"],params["id"])
+    db.execute("UPDATE blogposts SET BlogTitle = ?,BlogText = ? WHERE Id = ?",params["blog_title"],params["blog_text"],params["id"])
     redirect("/profile/#{session[:userid]}")
 end
 
 post('/:id/delete') do
     db = SQLite3::Database.new("db/blog.db")
-    db.execute("DELETE FROM blogposts WHERE id = (?)", params["id"])
-    id = "test"
+    db.execute("DELETE FROM blogposts WHERE Id = (?)", params["id"])
     redirect("/profile/#{session[:userid]}")
 end
 
 get('/profile/:id/edit') do
     db = SQLite3::Database.new('db/blog.db')
     db.results_as_hash = true
-    result = db.execute("SELECT Id, Username, Email FROM users WHERE id = ?",params["id"])
+    result = db.execute("SELECT Id, Username, Email FROM users WHERE Id = ?",params["id"])
     slim(:editprofile, locals:{result: result})
 end
 
 post('/profile/:id/update') do
     db = SQLite3::Database.new('db/blog.db')
     if params["password"] == ""
-	    db.execute("UPDATE users set Username = ?, Email = ?, WHERE id = ?",params["username"],params["email"],params["id"])    
+	    db.execute("UPDATE users set Username = ?, Email = ?, WHERE Id = ?",params["username"],params["email"],params["id"])    
     else
         hashedpassword = BCrypt::Password.create(params["password"])
         # if db.execute("Select username FROM users WHERE username =?",params["username"]) == params["username"] or db.execute("Select username FROM users WHERE username =?",params["username"]) != []
@@ -150,8 +150,8 @@ post('/profile/:id/update') do
         #     redirect(back)
         # else
         # end
-        # TODO fix duplicate username check
-        db.execute("UPDATE users set Username = ?, Hash = ?, Email = ?, WHERE id = ?",params["username"],hashedpassword,params["email"],params["id"])    
+        # TODO fix duplicate username and email check
+        db.execute("UPDATE users set Username = ?, Hash = ?, Email = ? WHERE Id = ?",params["username"],hashedpassword,params["email"],params["id"])    
     end
     session[:name] = params["name"]
     session[:username] = params["username"]
